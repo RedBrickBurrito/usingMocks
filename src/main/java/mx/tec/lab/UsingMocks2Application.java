d9aa3a3b329c062ff285d8a3786157cdb23e5579
Eduardo Almanza <eddy@localhost.localdomain>
2021-03-02 22:37:49 -0700

    first commit

---
+++ src/main/java/mx/tec/lab/UsingMocks2Application.java
@@ -0,0 +1,13 @@
+package mx.tec.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingMocks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingMocks2Application.class, args);
+	}
+
+}
